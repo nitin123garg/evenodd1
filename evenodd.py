5f619162e2c92768903e905cd17be3333c4badd7
nitin123garg <95nitingarg@gmail.com>
2020-09-13 21:37:37 -0700

    this is first version of even odd program

---
+++ evenodd.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3.8
+import time
+for i in range(1,21):
+    if i%2==0:
+        print("even number :",i)
+        time.sleep(1)
+    else:
+        print("odd number :",i)
+        time.sleep(2)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# this is my third repo
+
+## in this repo make a even odd program
+
+	python version 3.8.5
